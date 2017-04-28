2065243a2919e63277883bcf67b817019c343684
Untitled <littleluluno01@163.com>
2017-04-28 16:39:04 +0800

    fix style

---
+++ js/disableHistory.js
@@ -0,0 +1,17 @@
+/*
+src: http://www.cnblogs.com/webzwf/p/5714385.html
+*/
+
+$(function()
+{
+  if(window.history && window.history.pushState)
+  {
+    $(window).on('popstate', function()
+    {
+      window.history.pushState('forward', null, '#');
+      window.history.forward(1);
+    });
+  }
+  window.history.pushState('forward', null, '#'); //在IE中必须得有这两行
+  window.history.forward(1);
+});
